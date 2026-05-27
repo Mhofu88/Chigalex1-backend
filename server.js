6c1280f692d4a895df599ec46ca0e4051bc44e18
Mhofu88 <achigodo@gmail.com>
2026-05-27 08:22:32 +0200

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-app.get('/run-migration', async (req, res) => {
-  if (req.query.key !== process.env.ADMIN_KEY) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  // paste the contents of migrateFAQs(), migrateQuestions(), migrateMembers() here
-  // then remove this route after running
-});
