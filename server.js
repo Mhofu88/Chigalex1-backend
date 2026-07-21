be30ab5f0ad399baf78da371ff79a54053f0553a
Mhofu88 <achigodo@gmail.com>
2026-07-21 10:22:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const subscriptionsRouter = require("./subscriptions-admin");
+app.use("/", subscriptionsRouter);
 
 app.use(cors());
 app.use(express.json());
